6be700f6e88f5452b389d68193004f187ad99581
Tony Stark <ironman0x7b2@protonmail.com>
2024-07-16 01:05:43 +0530

    feat: updated session and subscription queries to v3 version

--- client/subscription.go
+++ client/subscription.go
@@ -4,8 +4,8 @@ import (
 	"context"
 
 	cosmossdk "github.com/cosmos/cosmos-sdk/types"
-	base "github.com/sentinel-official/hub/v12/types"
 	"github.com/sentinel-official/hub/v12/x/subscription/types/v2"
+	"github.com/sentinel-official/hub/v12/x/subscription/types/v3"
 
 	"github.com/sentinel-official/sentinel-go-sdk/v1/client/options"
 )
@@ -15,28 +15,21 @@ const (
 	methodQuerySubscription            = "/sentinel.subscription.v2.QueryService/QuerySubscription"
 	methodQuerySubscriptions           = "/sentinel.subscription.v2.QueryService/QuerySubscriptions"
 	methodQuerySubscriptionsForAccount = "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForAccount"
-	methodQuerySubscriptionsForNode    = "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForNode"
 	methodQuerySubscriptionsForPlan    = "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForPlan"
 
 	// gRPC methods for querying subscription allocation information
 	methodQuerySubscriptionAllocation  = "/sentinel.subscription.v2.QueryService/QueryAllocation"
 	methodQuerySubscriptionAllocations = "/sentinel.subscription.v2.QueryService/QueryAllocations"
-
-	// gRPC methods for querying subscription payout information
-	methodQuerySubscriptionPayout            = "/sentinel.subscription.v2.QueryService/QueryPayout"
-	methodQuerySubscriptionPayouts           = "/sentinel.subscription.v2.QueryService/QueryPayouts"
-	methodQuerySubscriptionPayoutsForAccount = "/sentinel.subscription.v2.QueryService/QueryPayoutsForAccount"
-	methodQuerySubscriptionPayoutsForNode    = "/sentinel.subscription.v2.QueryService/QueryPayoutsForNode"
 )
 
 // Subscription queries and returns information about a specific subscription based on the provided subscription ID.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QuerySubscription" endpoint.
-// The result is a v2.Subscription and an error if the query fails.
-func (c *Context) Subscription(ctx context.Context, id uint64, opts *options.QueryOptions) (res v2.Subscription, err error) {
+// It uses gRPC to send a request to the "/sentinel.subscription.v3.QueryService/QuerySubscription" endpoint.
+// The result is a v3.Subscription and an error if the query fails.
+func (c *Context) Subscription(ctx context.Context, id uint64, opts *options.QueryOptions) (res *v3.Subscription, err error) {
 	// Initialize variables for the query.
 	var (
-		resp v2.QuerySubscriptionResponse
-		req  = &v2.QuerySubscriptionRequest{
+		resp v3.QuerySubscriptionResponse
+		req  = &v3.QuerySubscriptionRequest{
 			Id: id,
 		}
 	)
@@ -46,22 +39,18 @@ func (c *Context) Subscription(ctx context.Context, id uint64, opts *options.Que
 		return nil, err
 	}
 
-	// Unpack the response and return the subscription and a nil error.
-	if err := c.UnpackAny(resp.Subscription, &res); err != nil {
-		return nil, err
-	}
-
-	return res, nil
+	// Return a pointer to the subscription and a nil error.
+	return &resp.Subscription, nil
 }
 
 // Subscriptions queries and returns a list of subscriptions based on the provided options.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QuerySubscriptions" endpoint.
-// The result is a slice of v2.Subscription and an error if the query fails.
-func (c *Context) Subscriptions(ctx context.Context, opts *options.QueryOptions) (res []v2.Subscription, err error) {
+// It uses gRPC to send a request to the "/sentinel.subscription.v3.QueryService/QuerySubscriptions" endpoint.
+// The result is a slice of v3.Subscription and an error if the query fails.
+func (c *Context) Subscriptions(ctx context.Context, opts *options.QueryOptions) (res []v3.Subscription, err error) {
 	// Initialize variables for the query.
 	var (
-		resp v2.QuerySubscriptionsResponse
-		req  = &v2.QuerySubscriptionsRequest{
+		resp v3.QuerySubscriptionsResponse
+		req  = &v3.QuerySubscriptionsRequest{
 			Pagination: opts.PageRequest(),
 		}
 	)
@@ -71,26 +60,19 @@ func (c *Context) Subscriptions(ctx context.Context, opts *options.QueryOptions)
 		return nil, err
 	}
 
-	// Unpack each subscription in the response and return the list of subscriptions and a nil error.
-	res = make([]v2.Subscription, len(resp.Subscriptions))
-	for i := 0; i < len(resp.Subscriptions); i++ {
-		if err := c.UnpackAny(resp.Subscriptions[i], &res[i]); err != nil {
-			return nil, err
-		}
-	}
-
-	return res, nil
+	// Return the list of subscriptions and a nil error.
+	return resp.Subscriptions, nil
 }
 
 // SubscriptionsForAccount queries and returns a list of subscriptions associated with a specific account.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForAccount" endpoint.
-// The result is a slice of v2.Subscription and an error if the query fails.
+// It uses gRPC to send a request to the "/sentinel.subscription.v3.QueryService/QuerySubscriptionsForAccount" endpoint.
+// The result is a slice of v3.Subscription and an error if the query fails.
 // The account is identified by the provided cosmossdk.AccAddress.
-func (c *Context) SubscriptionsForAccount(ctx context.Context, accAddr cosmossdk.AccAddress, opts *options.QueryOptions) (res []v2.Subscription, err error) {
+func (c *Context) SubscriptionsForAccount(ctx context.Context, accAddr cosmossdk.AccAddress, opts *options.QueryOptions) (res []v3.Subscription, err error) {
 	// Initialize variables for the query.
 	var (
-		resp v2.QuerySubscriptionsForAccountResponse
-		req  = &v2.QuerySubscriptionsForAccountRequest{
+		resp v3.QuerySubscriptionsForAccountResponse
+		req  = &v3.QuerySubscriptionsForAccountRequest{
 			Address:    accAddr.String(),
 			Pagination: opts.PageRequest(),
 		}
@@ -101,56 +83,19 @@ func (c *Context) SubscriptionsForAccount(ctx context.Context, accAddr cosmossdk
 		return nil, err
 	}
 
-	// Unpack each subscription in the response and return the list of subscriptions and a nil error.
-	res = make([]v2.Subscription, len(resp.Subscriptions))
-	for i := 0; i < len(resp.Subscriptions); i++ {
-		if err := c.UnpackAny(resp.Subscriptions[i], &res[i]); err != nil {
-			return nil, err
-		}
-	}
-
-	return res, nil
-}
-
-// SubscriptionsForNode queries and returns a list of subscriptions associated with a specific node.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForNode" endpoint.
-// The result is a slice of v2.Subscription and an error if the query fails.
-// The node is identified by the provided base.NodeAddress.
-func (c *Context) SubscriptionsForNode(ctx context.Context, nodeAddr base.NodeAddress, opts *options.QueryOptions) (res []v2.Subscription, err error) {
-	// Initialize variables for the query.
-	var (
-		resp v2.QuerySubscriptionsForNodeResponse
-		req  = &v2.QuerySubscriptionsForNodeRequest{
-			Address:    nodeAddr.String(),
-			Pagination: opts.PageRequest(),
-		}
-	)
-
-	// Send a gRPC query using the provided context, method, request, response, and options.
-	if err := c.QueryGRPC(ctx, methodQuerySubscriptionsForNode, req, &resp, opts); err != nil {
-		return nil, err
-	}
-
-	// Unpack each subscription in the response and return the list of subscriptions and a nil error.
-	res = make([]v2.Subscription, len(resp.Subscriptions))
-	for i := 0; i < len(resp.Subscriptions); i++ {
-		if err := c.UnpackAny(resp.Subscriptions[i], &res[i]); err != nil {
-			return nil, err
-		}
-	}
-
-	return res, nil
+	// Return the list of subscriptions and a nil error.
+	return resp.Subscriptions, nil
 }
 
 // SubscriptionsForPlan queries and returns a list of subscriptions associated with a specific plan.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QuerySubscriptionsForPlan" endpoint.
-// The result is a slice of v2.Subscription and an error if the query fails.
+// It uses gRPC to send a request to the "/sentinel.subscription.v3.QueryService/QuerySubscriptionsForPlan" endpoint.
+// The result is a slice of v3.Subscription and an error if the query fails.
 // The plan is identified by the provided ID.
-func (c *Context) SubscriptionsForPlan(ctx context.Context, id uint64, opts *options.QueryOptions) (res []v2.Subscription, err error) {
+func (c *Context) SubscriptionsForPlan(ctx context.Context, id uint64, opts *options.QueryOptions) (res []v3.Subscription, err error) {
 	// Initialize variables for the query.
 	var (
-		resp v2.QuerySubscriptionsForPlanResponse
-		req  = &v2.QuerySubscriptionsForPlanRequest{
+		resp v3.QuerySubscriptionsForPlanResponse
+		req  = &v3.QuerySubscriptionsForPlanRequest{
 			Id:         id,
 			Pagination: opts.PageRequest(),
 		}
@@ -161,15 +106,8 @@ func (c *Context) SubscriptionsForPlan(ctx context.Context, id uint64, opts *opt
 		return nil, err
 	}
 
-	// Unpack each subscription in the response and return the list of subscriptions and a nil error.
-	res = make([]v2.Subscription, len(resp.Subscriptions))
-	for i := 0; i < len(resp.Subscriptions); i++ {
-		if err := c.UnpackAny(resp.Subscriptions[i], &res[i]); err != nil {
-			return nil, err
-		}
-	}
-
-	return res, nil
+	// Return the list of subscriptions and a nil error.
+	return resp.Subscriptions, nil
 }
 
 // SubscriptionAllocation queries and returns information about a specific allocation within a subscription.
@@ -214,92 +152,4 @@ func (c *Context) SubscriptionAllocations(ctx context.Context, id uint64, opts *
 
 	// Return the list of allocations and a nil error.
 	return resp.Allocations, nil
-}
-
-// SubscriptionPayout queries and returns information about a specific payout within a subscription.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QueryPayout" endpoint.
-// The result is a pointer to v2.Payout and an error if the query fails.
-func (c *Context) SubscriptionPayout(ctx context.Context, id uint64, opts *options.QueryOptions) (res *v2.Payout, err error) {
-	// Initialize variables for the query.
-	var (
-		resp v2.QueryPayoutResponse
-		req  = &v2.QueryPayoutRequest{
-			Id: id,
-		}
-	)
-
-	// Send a gRPC query using the provided context, method, request, response, and options.
-	if err := c.QueryGRPC(ctx, methodQuerySubscriptionPayout, req, &resp, opts); err != nil {
-		return nil, err
-	}
-
-	// Return a pointer to the payout and a nil error.
-	return &resp.Payout, nil
-}
-
-// SubscriptionPayouts queries and returns a list of payouts within a specific subscription.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QueryPayouts" endpoint.
-// The result is a slice of v2.Payout and an error if the query fails.
-func (c *Context) SubscriptionPayouts(ctx context.Context, opts *options.QueryOptions) (res []v2.Payout, err error) {
-	// Initialize variables for the query.
-	var (
-		resp v2.QueryPayoutsResponse
-		req  = &v2.QueryPayoutsRequest{
-			Pagination: opts.PageRequest(),
-		}
-	)
-
-	// Send a gRPC query using the provided context, method, request, response, and options.
-	if err := c.QueryGRPC(ctx, methodQuerySubscriptionPayouts, req, &resp, opts); err != nil {
-		return nil, err
-	}
-
-	// Return the list of payouts and a nil error.
-	return resp.Payouts, nil
-}
-
-// SubscriptionPayoutsForAccount queries and returns a list of payouts associated with a specific account.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QueryPayoutsForAccount" endpoint.
-// The result is a slice of v2.Payout and an error if the query fails.
-// The account is identified by the provided cosmossdk.AccAddress.
-func (c *Context) SubscriptionPayoutsForAccount(ctx context.Context, accAddr cosmossdk.AccAddress, opts *options.QueryOptions) (res []v2.Payout, err error) {
-	// Initialize variables for the query.
-	var (
-		resp v2.QueryPayoutsForAccountResponse
-		req  = &v2.QueryPayoutsForAccountRequest{
-			Address:    accAddr.String(),
-			Pagination: opts.PageRequest(),
-		}
-	)
-
-	// Send a gRPC query using the provided context, method, request, response, and options.
-	if err := c.QueryGRPC(ctx, methodQuerySubscriptionPayoutsForAccount, req, &resp, opts); err != nil {
-		return nil, err
-	}
-
-	// Return the list of payouts and a nil error.
-	return resp.Payouts, nil
-}
-
-// SubscriptionPayoutsForNode queries and returns a list of payouts associated with a specific node.
-// It uses gRPC to send a request to the "/sentinel.subscription.v2.QueryService/QueryPayoutsForNode" endpoint.
-// The result is a slice of v2.Payout and an error if the query fails.
-// The node is identified by the provided base.NodeAddress.
-func (c *Context) SubscriptionPayoutsForNode(ctx context.Context, nodeAddr base.NodeAddress, opts *options.QueryOptions) (res []v2.Payout, err error) {
-	// Initialize variables for the query.
-	var (
-		resp v2.QueryPayoutsForNodeResponse
-		req  = &v2.QueryPayoutsForNodeRequest{
-			Address:    nodeAddr.String(),
-			Pagination: opts.PageRequest(),
-		}
-	)
-
-	// Send a gRPC query using the provided context, method, request, response, and options.
-	if err := c.QueryGRPC(ctx, methodQuerySubscriptionPayoutsForNode, req, &resp, opts); err != nil {
-		return nil, err
-	}
-
-	// Return the list of payouts and a nil error.
-	return resp.Payouts, nil
 }
